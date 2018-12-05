91b61e890a4409b8842f8e93b8a50c03de6697e6
Park-In-Geun <newxenesis2@naver.com>
2018-12-05 15:30:54 +0900

    arrange file

--- CloudEditer/gtk_src/gtk-notepad.h
+++ CloudEditer/gtk_src/gtk-notepad.h
@@ -1,14 +1,14 @@
 #include <gtk/gtk.h>
-#include <gdk/gdkkeysys.h>
-#define NO_FILE_LOADED (loaded_fn[0]== '\0')
+#include <gdk/gdkkeysyms.h>
+#define NO_FILE_LOADED (loaded_fn[0] == '\0')
 
-char *NAME;
-char *title = "GTK Notepad";
-char *icon = "gtk-notepad.png";
+char *PROGNAME;
+char *title = "Untitled - GTK Notepad";
+char *icon  = "gtk-notepad.png";
 char *loaded_fn = NULL;
 int errno;
 
-//Main window
+// Main window
 GtkWidget *window, *vbox;
 GtkWindow *gwindow;
 GtkAccelGroup *accel = NULL;
@@ -17,29 +17,29 @@ GtkWidget *statusbar;
 GdkAtom atom;
 GtkClipboard *clipboard;
 
-//Menubar
+// Menubar
 GtkWidget *menubar, *sep;
 
-//File menu
+// File menu
 GtkWidget *filemenu, *file;
 GtkWidget *new, *open, *save, *saveas, *quit;
 
-//Edit menu
+// Edit menu
 GtkWidget *editmenu, *edit;
 GtkWidget *cut, *copy, *paste, *delete;
-GtkWidget *selectall, *time_data;
+GtkWidget *selectall, *time_date;
 GtkWidget *wll, *select_font;
 
-//Help menu
+// Help menu
 GtkWidget *helpmenu, *help;
 GtkWidget *about;
 
-//GtkTextView
+// GtkTextView
 GtkWidget *textarea;
 GtkTextBuffer *buffer;
 GtkWidget *scrolledwindow;
 gboolean modified = FALSE;
-char wrapping = 1; //Wrapping will be set to 0 by defalut
+char wrapping = 1; // Wrapping will be set to 0 by default
 
 void gtk_statusbar_update_lncol(void);
 char gtk_notepad_open_file(const char*);
